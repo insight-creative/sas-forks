42aff0136c025a42487e0bc5c0168146c21e713f
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-07-30 08:00:09 -0500

    Sketch in home page content

--- front-page.php
+++ front-page.php
@@ -1,5 +1,41 @@
 <?php get_header(); ?>
 
-<h1>Hello, world!</h1>
+<section class="container-narrow text-center">
+  <p>SAS FORKS™ and attachments are built to the rigorous specifications resulting in a tool tough enough to handle severe conditions and heavier loads, this gives operators performance, durability, and an extended useful life. The ultimate the goal of SAS is to eliminate down time.</p>
+</section>
+
+<section class="container">
+  <h2 class="b text-center">Our core services</h2>
+  <hr>
+  <h3 class="b">Our specialty</h3>
+  <p>We specialize in manufacturing 6' to 18' fork assemblies for use in the auto salvage industry. Our extensive experience in the auto salvage market, where operators are especially hard on their equipment, has helped us develop insights into other industry segments which demand performance from their forks in the most extreme environments.</p>
+
+  <h3 class="b">Built for your equipment</h3>
+  <p>SAS FORKS and attachments are manufactured to fit many types of equipment, including wheel loaders, excavators and forklifts(fork trucks). We engineer custom products for telehandlers, skid steers and backhoes.</p>
+
+  <h3 class="b">Multiple applications</h3>
+  <p>Products are delivered with a wide variety of functions. Fixed and adjustable widths of forks and carriages as well as the SAS Quick Coupler System for wheel loaders, for use in multiple material handling applications including rock quarries, oil fields, steel mills and defense applications.</p>
+</section>
+
+<section>
+  <div class="container">
+    <h2>Product spotlight<br>Dumpsters for wheel loaders</h2>
+    <p>Use as collection bins for engines, transmissions, wheels, wire and or cores. Smaller sizes save space in the processing area on site.</p>
+    <a href="#">Learn more</a>
+  </div>
+</section>
+
+<section class="container">
+  <h2>Testimonials</h2>
+</section>
+
+<section>
+  <div class="container text-center">
+    <h2>Have questions?</h2>
+    <p>We're here to help you—contact SAS Forks today!</p>
+    <p>(877) 727-3675</p>
+    <a href="/contact">Contact us now</a>
+  </div>
+</section>
 
 <?php get_footer(); ?>
