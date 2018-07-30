f8abefb625966d2fe888670e8834707324f94b99
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-07-30 08:49:20 -0500

    Sketch in blog roll index

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <?php get_header(); ?>
 
-<h1>Blog</h1>
+<section class="jumbotron text-center">
+  <div class="container">
+    <h1 class="jumbotron-title">SAS News</h1>
+  </div>
+</section>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-  <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-  <span><?php echo get_the_time('F jS, Y'); ?></span>
-  <?php if (has_post_thumbnail(get_the_id())) : ?>
-    <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('full'); ?></a>
+<section class="container">
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div class="card">
+      <?php if (has_post_thumbnail(get_the_id())) : ?>
+        <a class="image-link" href="<?php the_permalink() ?>"><?php the_post_thumbnail('full'); ?></a>
+      <?php endif; ?>
+      <h2 class="card-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+      <span class="card-meta"><?php echo get_the_time('F jS, Y'); ?></span>
+      <?php the_excerpt(); ?>
+    </div>
+  <?php endwhile; else: ?>
+    <p class="tc"><?php _e('Blog coming soon!'); ?></p>
   <?php endif; ?>
-  <?php the_excerpt(); ?>
-<?php endwhile; else: ?>
-  <p class="tc"><?php _e('Blog coming soon!'); ?></p>
-<?php endif; ?>
 
-<?php previous_posts_link( 'Newer posts' ); ?>
-<?php next_posts_link( 'Older posts' ); ?>
+  <?php previous_posts_link( 'Newer posts' ); ?>
+  <?php next_posts_link( 'Older posts' ); ?>
+</section>
 
 <?php get_footer(); ?>
--- modules/blog_links.php
+++ modules/blog_links.php
@@ -9,7 +9,7 @@ add_filter('next_posts_link_attributes', 'posts_link_attributes');
 add_filter('previous_posts_link_attributes', 'posts_link_attributes');
 
 function new_excerpt_more( $more ) {
-	return '... <a class="" href="'. get_permalink($post->ID) . '">Read More</a>';
+	return '...<br><a class="btn btn-primary" href="'. get_permalink($post->ID) . '">Read More</a>';
 }
 add_filter( 'excerpt_more', 'new_excerpt_more' );
 
