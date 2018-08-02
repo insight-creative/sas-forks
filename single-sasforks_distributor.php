e9d97f0d66f33ce209c7dcfffd9b0edcda883a33
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-08-02 11:06:44 -0500

    Style contact templates

--- single-sasforks_distributor.php
+++ single-sasforks_distributor.php
@@ -1,28 +1,50 @@
-<?php get_header(); ?>
-
-<h1><?php the_title(); ?></h1>
-
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-<?php the_content(); ?>
-
 <?php
-  $contactId = get_field('contact_form')[0]->ID;
-  echo do_shortcode("[contact-form-7 id='{$contactId}']");
+  get_header();
+  if ( have_posts() ) : while ( have_posts() ) : the_post();
 ?>
 
-<address>
-  <strong><?php the_field('name'); ?></strong><br>
-  <?php the_field('address'); ?>
-</address>
+<?php
+  if (has_post_thumbnail( $post->ID ) ) {
+    $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail');
+  }
+?>
 
-<p>
-  Email: <a href="mailto:<?php the_field('contact_email'); ?>"><?php the_field('contact_name'); ?></a><br>
-  Phone: <?php the_field('contact_phone_number'); ?><br>
-  <?php if (the_field('contact_fax')): ?>Fax: <?php the_field('contact_fax'); ?><br><?php endif; ?>
-  <a href="<?php the_field('contact_website'); ?>">Visit our website</a>
-</p>
+<section class="jumbotron text-center" style="background-image: url(<?php echo $image[0]; ?>);">
+  <div class="container">
+    <h1 class="jumbotron-title"><?php the_title(); ?></h1>
+  </div>
+</section>
 
-<?php endwhile; endif; ?>
+<section class="container-narrow">
+  <?php the_content(); ?>
+</section>
+
+<section class="container cf">
+  <div class="col-50">
+    <?php
+      $contactId = get_field('contact_form')[0]->ID;
+      echo do_shortcode("[contact-form-7 id='{$contactId}']");
+    ?>
+  </div>
+
+  <div class="col-50">
+    <img src="<?php the_field('distributor_logo'); ?>" alt="<?php the_field('contact_name'); ?>">
+    <address>
+      <strong><?php the_field('name'); ?></strong><br>
+      <?php the_field('address'); ?>
+    </address>
+
+    <p>
+      Email: <a href="mailto:<?php the_field('contact_email'); ?>"><?php the_field('contact_name'); ?></a><br>
+      Phone: <?php the_field('contact_phone_number'); ?><br>
+      <?php if (get_field('contact_fax')): ?>Fax: <?php the_field('contact_fax'); ?><br><?php endif; ?>
+      <a href="<?php the_field('contact_website'); ?>">Visit our website</a>
+    </p>
+  </div>
+
+  <?php endwhile; else: ?>
+  	<p><?php _e('Sorry, this page does not exist.'); ?></p>
+  <?php endif; ?>
+</section>
 
 <?php get_footer(); ?>
