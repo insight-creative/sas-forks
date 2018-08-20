068ae5e78547256e69c80039723f3f3ad5b5e245
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-08-19 21:59:17 -0500

    Fix font link

--- modules/styles.php
+++ modules/styles.php
@@ -4,7 +4,7 @@
 function theme_styles() {
 
   // If loading fonts from Google Fonts or Typekit, put URL here
-  $font_url = 'https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i';
+  $font_url = 'https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i,800,800i';
 
   // Gets the theme version and automatically appends it to the stylesheet url
   $theme = wp_get_theme();
