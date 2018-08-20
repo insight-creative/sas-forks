06e9d80bb417777959681e963a202d1822ba2358
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-08-19 20:38:54 -0500

    Fix script and style version numbers

--- modules/styles.php
+++ modules/styles.php
@@ -5,11 +5,11 @@
 // If loading fonts from Google Fonts or Typekit, put URL here
 $font_url = 'https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i';
 
-// Gets the theme version and automatically appends it to the stylesheet url
-$theme = wp_get_theme();
-$version = $theme->get('Version');
-
 function theme_styles() {
+
+  // Gets the theme version and automatically appends it to the stylesheet url
+  $theme = wp_get_theme();
+  $version = $theme->get('Version');
 
   wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
 
