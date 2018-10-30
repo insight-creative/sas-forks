7f94b48034eb3c8b149e573b2b9e492a88aa0f53
Justin-Insight <jparsons@insightcreative.com>
2018-10-30 11:21:13 -0500

    added v=2 to script call to try to force break of browser cache

--- modules/scripts.php
+++ modules/scripts.php
@@ -8,8 +8,8 @@ function theme_scripts() {
 	$theme = wp_get_theme();
 	$version = $theme->get('Version');
 
-	wp_enqueue_script( 'main', get_template_directory_uri() . '/scripts/main.js', array( 'jquery' ), $version, true);
-	
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/scripts/main.js?v=2', array( 'jquery' ), $version, true);
+
 }
 add_action( 'wp_enqueue_scripts', 'theme_scripts' );
 
