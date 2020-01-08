22a50e341f7d8505a97adfff44cb6d9564b13ca7
Justin-Insight <jparsons@insightcreative.com>
2020-01-08 10:08:17 -0600

    removed the featured image of the latest blog post so it only shows the video, not both image and video

--- single-sasforks_distributor.php
+++ single-sasforks_distributor.php
@@ -20,39 +20,8 @@
     <?php the_content(); ?>
   </section>
 
-<<<<<<< HEAD
   <section class="container cf flex-container">
-=======
   <section class="container cf flex-container-reverse">
->>>>>>> aa4a6441f4110bb4ab097c9a121178bcdcd76891
-    <div class="col-50">
-      <?php
-        $contactId = get_field('contact_form')[0]->ID;
-        echo do_shortcode("[contact-form-7 id='{$contactId}']");
-      ?>
-    </div>
-
-    <div class="col-50">
-      <?php if (get_field('distributor_logo')): ?>
-        <img src="<?php the_field('distributor_logo'); ?>" alt="<?php the_field('name'); ?>">
-      <?php endif; ?>
-      <address>
-        <strong><?php the_field('name'); ?></strong><br>
-        <?php the_field('address'); ?>
-      </address>
-
-      <p>
-        Email: <a href="mailto:<?php the_field('contact_email'); ?>?subject=Inquiry%20from%20sasforks.com"><?php the_field('contact_name'); ?></a> <a href="mailto:<?php the_field('contact_2_email'); ?>?subject=Inquiry%20from%20sasforks.com"><?php the_field('contact_2_name'); ?></a><br>
-        Phone: <?php the_field('contact_phone_number'); ?><br>
-        <?php if (get_field('contact_fax')): ?>Fax: <?php the_field('contact_fax'); ?><br><?php endif; ?>
-        <a href="<?php the_field('contact_website');?>">Website</a>
-      </p>
-    </div>
-
-    <?php endwhile; else: ?>
-    	<p><?php _e('Sorry, this page does not exist.'); ?></p>
-    <?php endif; ?>
-  </section>
 </main>
 
 <?php get_footer(); ?>
